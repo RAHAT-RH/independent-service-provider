9cc709cf427b960936f6a40626b7865c56b65888
RAHAT-RHT <alaminrahat12@gmail.com>
2022-04-18 05:53:07 +0600

    successfully reset password

--- src/component/ResetPassword/ResetPassword.js
+++ src/component/ResetPassword/ResetPassword.js
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
-import { ToastContainer } from 'react-toastify';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import { toast, ToastContainer } from 'react-toastify';
+import auth from '../../fireBase.init';
 
 const ResetPassword = () => {
     const [email, setEmail] = useState('');
     const handleEmail = (event) =>{
         setEmail(event.target.value)
     }
+    const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(auth);
     
     const handleResetPassword = () => {
-        
+        sendPasswordResetEmail(email);
+        toast('Reset Password')
     }
     return (
         <div>
             <div className="login-container" style={{height: "400px"}}>
             <div className="login-title">Reset Password</div>
-            <form  className="login-form">
+            <form onSubmit={handleResetPassword} className="login-form">
                 <input onChange={handleEmail} type="text" placeholder="Your Email" />
-                {/* {errors?.emailError && <p className="error-message">{errors.emailError}</p>} */}
                 <button type='submit'>Reset Password</button>
                 
             </form>
